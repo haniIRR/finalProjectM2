77f08b24600fd2f4abaef8242f95645108174844
melobit <helmaeb26@gmail.com>
2023-01-11 14:13:23 +0330

    music play activity

--- app/src/main/java/com/kazemi/finalprojectm2/music_player.java
+++ app/src/main/java/com/kazemi/finalprojectm2/music_player.java
@@ -3,12 +3,28 @@ package com.kazemi.finalprojectm2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class music_player extends AppCompatActivity {
+
+    TextView currenttime,totaltime,songtitleplayer;
+    SeekBar seekBar;
+    ImageView puseplay,musicimg,nextbtn,prebtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music_player);
+
+        currenttime=findViewById(R.id.currenttime);
+        totaltime=findViewById(R.id.totaltime);
+        songtitleplayer=findViewById(R.id.songTitlePlayer);
+        seekBar=findViewById(R.id.seekbar);
+        puseplay=findViewById(R.id.pause_play);
+        musicimg=findViewById(R.id.img_music_player);
+        nextbtn=findViewById(R.id.nextbtn);
+        prebtn=findViewById(R.id.previousbtn);
     }
 }
