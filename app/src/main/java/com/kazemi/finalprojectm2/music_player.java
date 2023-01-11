abc8c01ab2c39a84d862ac6ef8511357ea27633b
melobit <helmaeb26@gmail.com>
2023-01-11 14:04:06 +0330

    music play activity

---
+++ app/src/main/java/com/kazemi/finalprojectm2/music_player.java
@@ -0,0 +1,14 @@
+package com.kazemi.finalprojectm2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class music_player extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music_player);
+    }
+}
